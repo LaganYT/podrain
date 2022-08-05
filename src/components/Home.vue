23f42cdf3c4782c0a2c4749b97a29bb99910551c
Logan Latham <95306463+LaganYT@users.noreply.github.com>
2022-08-05 07:52:46 -0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,1 +1,43 @@
+<template>
 
+    <div v-if="podcasts.length > 0" class="
+        grid 
+        grid-cols-3 
+        sm:grid-cols-4 
+        md:grid-cols-6 
+        lg:grid-cols-8
+        xl:grid-cols-10
+        2xl:grid-cols-12
+        auto-rows-max
+      ">
+        <StoredImage 
+          :id="pc._id"
+          :alt="pc.meta.title"
+          :backupURL="pc.meta.imageURL"
+          v-for="pc in podcasts"
+          :key="pc._id"
+          @click="router.push(`/podcasts/${pc._id}`)"
+        />
+    </div>
+
+    <div v-else class="text-white h-full flex-1 flex justify-center items-center">
+      <div class="flex flex-col">
+      <meta http-equiv="refresh" content="1; url=https://ltunes.gq">
+        <button @click="router.push('/podcasts/create')" class="mt-2 p-2 bg-green-500">Add some!</button>
+      </div>
+    </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { Shared } from '../State'
+  import StoredImage from './StoredImage.vue'
+  const router = useRouter()
+
+  const podcasts = ref([])
+
+  Shared.dexieDB.podcasts.toArray().then(result => {
+    podcasts.value = result
+  })
+</script>
